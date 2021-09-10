6747a1890bafa67269fe7d75a1e3578e448190eb
mayrazan <mayramanchein@yahoo.com.br>
2021-09-09 23:31:45 -0300

    feat(config): adding dependencies and creating theme

---
+++ src/theme/breakpoints.js
@@ -0,0 +1,9 @@
+const breakpoints = {
+  xs: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
+export default breakpoints;
---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import typographyFonts from './typographyFonts';
+import breakpoints from './breakpoints';
+import * as colors from './colors';
+
+export default {
+  colors,
+  typographyFonts,
+  breakpoints,
+  fontFamily: 'Mulish, sans-serif',
+  transition: '200ms ease-in-out',
+  borderRadius: '8px',
+};
