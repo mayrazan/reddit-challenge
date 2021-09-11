391b617f0e047636a0df77d1a7c57bf35ecfe779
mayrazan <mayramanchein@yahoo.com.br>
2021-09-11 14:34:46 -0300

    feat(button): creating button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,40 @@
+import styled, { css } from 'styled-components';
+import styleProps from '../../utils/receiveStyleProps';
+import { TextStyleVariants } from '../Text';
+
+const Button = styled.button`
+  color: ${({ theme, color }) => theme.text[color]};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: opacity ${({ theme }) => theme.transition};
+  background-color: ${({ theme }) => theme.background.gray};
+  ${TextStyleVariants('subtitle')}
+  width: 202px;
+  height: 48px;
+  border: 0;
+  cursor: pointer;
+  opacity: 1;
+
+  &:hover {
+    opacity: 0.5;
+  }
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.2;
+  }
+  ${({ fullWidth }) => fullWidth
+    && css`
+      width: 100%;
+      background-color: ${({ theme }) => theme.background.shadesPurple};
+    `};
+  
+
+  ${styleProps('margin')}
+  ${styleProps('display')}
+
+  ${({ isActive }) => isActive
+    && css`
+      background-color: ${({ theme }) => theme.background.shadesPurple};
+    `};
+`;
+
+export default Button;
