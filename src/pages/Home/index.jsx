1d2cea88cf31e5f5381c030ceccf66ba3b7fe161
mayrazan <mayramanchein@yahoo.com.br>
2021-09-12 00:21:29 -0300

    refactor(pages): refactoring pages to receive loader

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,37 +3,65 @@ import CardPost from '../../components/CardPost';
 import Button from '../../components/Button';
 import Footer from '../../components/Footer';
 import getPosts from '../../services/getPosts';
+import Loading from '../../components/Loading';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
 
   useEffect(() => {
-    getPosts().then((res) => {
-      setPosts(res.data);
-      setPage(res.next);
-    });
+    getPosts()
+      .then((res) => {
+        setPosts(res.data);
+        setPage(res.next);
+        setIsLoading(true);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+      });
   }, []);
 
   const seeMore = () => {
-    getPosts('hot', 25, page).then((res) => {
-      setPosts((prevState) => [...prevState, ...res.data]);
-      setPage(res.next);
-      window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth',
+    getPosts('hot', 25, page)
+      .then((res) => {
+        setIsLoadingMore(true);
+        setPosts((prevState) => [...prevState, ...res.data]);
+        setPage(res.next);
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: 'smooth',
+        });
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoadingMore(false);
+        }, 300);
       });
-    });
   };
 
   return (
     <>
-      <CardPost posts={posts} />
-      <Footer>
-        <Button color="button" fullWidth onClick={seeMore}>
-          + Ver mais
-        </Button>
-      </Footer>
+      {isLoading ? (
+        <Loading size={80} />
+      ) : (
+        <>
+          <CardPost posts={posts} />
+          <Footer>
+            <Button
+              color="button"
+              fullWidth
+              onClick={seeMore}
+              disabled={isLoadingMore || !page}
+            >
+              {!isLoadingMore ? '+ Ver mais' : <Loading size={30} />}
+            </Button>
+          </Footer>
+        </>
+      )}
     </>
   );
 };
--- src/pages/Rising/index.jsx
+++ src/pages/Rising/index.jsx
@@ -3,37 +3,68 @@ import CardPost from '../../components/CardPost';
 import Button from '../../components/Button';
 import Footer from '../../components/Footer';
 import getPosts from '../../services/getPosts';
+import Loading from '../../components/Loading';
 
 const Rising = () => {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
 
   useEffect(() => {
-    getPosts('rising', 1).then((res) => {
-      setPosts(res.data);
-      setPage(res.next);
-    });
+    getPosts('rising', 1)
+      .then((res) => {
+        setPosts(res.data);
+        setPage(res.next);
+        setIsLoading(true);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoadingMore(false);
+        }, 300);
+      });
   }, []);
 
   const seeMore = () => {
     getPosts('rising', 25, page).then((res) => {
+      setIsLoadingMore(true);
       setPosts((prevState) => [...prevState, ...res.data]);
       setPage(res.next);
       window.scrollTo({
         top: document.body.scrollHeight,
         behavior: 'smooth',
       });
+    }).finally(() => {
+      setTimeout(() => {
+        setIsLoadingMore(false);
+      }, 300);
     });
   };
 
   return (
     <>
-      <CardPost posts={posts} />
-      <Footer>
-        <Button color="button" fullWidth onClick={seeMore}>
-          + Ver mais
-        </Button>
-      </Footer>
+      {isLoading ? (
+        <Loading size={80} />
+      ) : (
+        <>
+          <CardPost posts={posts} />
+          <Footer>
+            <Button
+              color="button"
+              fullWidth
+              onClick={seeMore}
+              disabled={isLoadingMore || !page}
+            >
+              {!isLoadingMore ? '+ Ver mais' : <Loading size={30} />}
+            </Button>
+          </Footer>
+        </>
+      )}
     </>
   );
 };
